abf236c0f627f1da93f48565d3c2094c983ee2ca
bala-amypo <bala@amypo.in>
2025-12-20 08:26:25 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudAlertRecordService.java
+++ src/main/java/com/example/demo/service/FraudAlertRecordService.java
@@ -1,4 +1,11 @@
-package com.example.demo.model;
- public class DeviceOwnershipRecord{
-    
- }
+package com.example.demo.service;
+
+import com.example.demo.entity.FraudAlertRecord;
+import java.util.List;
+
+public interface FraudAlertService {
+
+    FraudAlertRecord createAlert(FraudAlertRecord alert);
+
+    List<FraudAlertRecord> getAllAlerts();
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,11 @@
-package com.example.demo.model;
- public class DeviceOwnershipRecord{
-    
- }
+package com.example.demo.service;
+
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.AuthRequest;
+
+public interface UserService {
+
+    void registerUser(RegisterRequest req);
+
+    String loginUser(AuthRequest req);
+}
