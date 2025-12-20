a2563aa52d6ed36e5a38766899d14971767de80d
bala-amypo <bala@amypo.in>
2025-12-20 08:32:24 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,4 +1,20 @@
-package com.example.demo.model;
- public class DeviceOwnershipRecord{
-    
- }
+package com.example.demo.service.impl;
+
+import com.example.demo.service.UserService;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.AuthRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void registerUser(RegisterRequest req) {
+        // simple
+    }
+
+    @Override
+    public String loginUser(AuthRequest req) {
+        return "TOKEN";
+    }
+}
