16a8f07aab16f14d5bb07fe447e7390801d52b97
bala-amypo <bala@amypo.in>
2025-12-20 08:17:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/StolenDeviceController.java
+++ src/main/java/com/example/demo/controller/StolenDeviceController.java
@@ -1,4 +1,25 @@
-package com.example.demo.model;
- public class DeviceOwnershipRecord{
-    
- }
+package com.example.demo.controller;
+
+import com.example.demo.service.StolenDeviceService;
+import com.example.demo.entity.StolenDeviceReport;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stolen-devices")
+public class StolenDeviceController {
+
+    @Autowired
+    private StolenDeviceService service;
+
+    @PostMapping
+    public StolenDeviceReport reportStolen(@RequestBody StolenDeviceReport report) {
+        return service.reportStolen(report);
+    }
+
+    @GetMapping
+    public List<StolenDeviceReport> getAllReports() {
+        return service.getAllReports();
+    }
+}
