e9fac57172ee14489805161d56510a057da4b56f
bala-amypo <bala@amypo.in>
2025-12-26 10:49:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider();
+    }
+}
+
