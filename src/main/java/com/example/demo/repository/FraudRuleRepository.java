efac1c62072b0b8b697609aa2b65a2439e6ca2f9
bala-amypo <bala@amypo.in>
2025-12-29 09:30:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/FraudRuleRepository.java
+++ src/main/java/com/example/demo/repository/FraudRuleRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 import com.example.demo.model.FraudRule;
 import java.util.*;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface FraudRuleRepository extends JpaRepository<FraudRule,Long> {
     Optional<FraudRule> findByRuleCode(String ruleCode);
--- src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.repository;
 import com.example.demo.model.WarrantyClaimRecord;
 import java.util.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
-public interface WarrantyClaimRecordRepository extends JpaRepository<Wa {
+public interface WarrantyClaimRecordRepository extends JpaRepository<WarrantyClaimRecord,Long> {
     boolean existsBySerialNumberAndClaimReason(String serial, String reason);
     Optional<WarrantyClaimRecord> findById(Long id);
     List<WarrantyClaimRecord> findBySerialNumber(String serial);
