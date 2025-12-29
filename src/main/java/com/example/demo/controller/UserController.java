ccba0ec3eba414fed7bf9b7dd4fccfc8ffd9bc5b
bala-amypo <bala@amypo.in>
2025-12-29 09:00:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,53 +1,53 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import org.springframework.web.bind.annotation.*;
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
+// import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/users")
-public class UserController {
+// @RestController
+// @RequestMapping("/api/users")
+// public class UserController {
 
-    private final UserService service;
+//     private final UserService service;
 
-    public UserController(UserService service) {
-        this.service = service;
-    }
+//     public UserController(UserService service) {
+//         this.service = service;
+//     }
 
 
-    @PostMapping
-    public User create(@RequestBody User user) {
-        return service.createUser(user);
-    }
+//     @PostMapping
+//     public User create(@RequestBody User user) {
+//         return service.createUser(user);
+//     }
 
  
-    @GetMapping("/{id}")
-    public Optional<User> getById(@PathVariable Long id) {
-        return service.getUserById(id);
-    }
+//     @GetMapping("/{id}")
+//     public Optional<User> getById(@PathVariable Long id) {
+//         return service.getUserById(id);
+//     }
 
-    @GetMapping("/email/{email}")
-    public Optional<User> getByEmail(@PathVariable String email) {
-        return service.getUserByEmail(email);
-    }
+//     @GetMapping("/email/{email}")
+//     public Optional<User> getByEmail(@PathVariable String email) {
+//         return service.getUserByEmail(email);
+//     }
 
-    @GetMapping
-    public List<User> getAll() {
-        return service.getAllUsers();
-    }
+//     @GetMapping
+//     public List<User> getAll() {
+//         return service.getAllUsers();
+//     }
 
-    @PutMapping("/{id}")
-    public User update(@PathVariable Long id,
-                       @RequestBody User user) {
-        return service.updateUser(id, user);
-    }
+//     @PutMapping("/{id}")
+//     public User update(@PathVariable Long id,
+//                        @RequestBody User user) {
+//         return service.updateUser(id, user);
+//     }
 
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long id) {
-        service.deleteUser(id);
-        return "User deleted successfully";
-    }
-}
+//     @DeleteMapping("/{id}")
+//     public String delete(@PathVariable Long id) {
+//         service.deleteUser(id);
+//         return "User deleted successfully";
+//     }
+// }
