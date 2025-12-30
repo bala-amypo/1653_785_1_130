ea6821b2d374b2a9c3c4b793c0e9086def3b7b40
bala-amypo <bala@amypo.in>
2025-12-30 04:44:32 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudAlertController.java
+++ src/main/java/com/example/demo/controller/FraudAlertController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
-
 import com.example.demo.model.FraudAlertRecord;
 import com.example.demo.service.FraudAlertService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
